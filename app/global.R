3d9ff8bb4f337d5511d5ca6004ccec62e9cc7ff8
catherineygao <77375906+catherineygao@users.noreply.github.com>
2021-02-13 11:25:00 -0800

    Upload App Package
    Created framework for Global R, UI, and Server. Feel free to add your section into the R files and make modifications where needed.

--- app/global.R
+++ app/global.R
@@ -1,27 +1,48 @@
-# install required packages
-if (!require("tidyverse")) {
-  install.packages("tidyverse")
-  library(tidyverse)
-}
+############# THIS FILE CONTAINS ALL REQUIRED PACKAGES AND DATA SETS##############
+
+#---------------Install required packages-------------------------------------
 if (!require("dplyr")) {
   install.packages("dplyr")
   library(dplyr)
 }
-if (!require("ggplot2")) {
-  install.packages("ggplot2")
-  library(ggplot2)
+if (!require("plotly")) {
+  install.packages("plotly")
+  library(plotly)
+}
+if (!require("shiny")) {
+  install.packages("shiny")
+  library(shiny)
+}
+if (!require("shinydashboard")) {
+  install.packages("shinydashboard")
+  library(shinydashboard)
 }
 
 
 
-#===============getting the food retail store data=========================================
 
-# download from link "https://data.ny.gov/Economic-Development/Retail-Food-Stores/9a8c-vfzj/data"
-foodstore <- read.csv('Retail_Food_Stores.csv')
-foodstore$Coordinate <- sapply(strsplit(foodstore$Location, '[()]'), '[', 2)
-foodstore$Latitude <- sapply(strsplit(foodstore$Coordinate, ', '), '[', 1)
-foodstore$Longitude <- sapply(strsplit(foodstore$Coordinate, ', '), '[', 2)
-head(foodstore)
+#---------------Load required data sets--------------------------------------
 
-# show list fo county and check for NA in Coordinate (we can probably omit these)
-table(foodstore$County, is.na(foodstore$Coordinate))
+# get the total case count in nyc
+nyc_total <- read.csv("https://raw.githubusercontent.com/nychealth/coronavirus-data/master/totals/summary.csv")
+
+# case count by zip code and boro
+data_by_modzcta<- read.csv("https://raw.githubusercontent.com/nychealth/coronavirus-data/master/totals/data-by-modzcta.csv")
+
+# summarize case count by boro
+data_by_boro <- data_by_modzcta %>% group_by(BOROUGH_GROUP) %>% summarise(CASE_COUNT = sum(COVID_CASE_COUNT))
+
+# covid case by borough and stats
+group_case_boro <- read.csv("https://raw.githubusercontent.com/nychealth/coronavirus-data/master/totals/group-cases-by-boro.csv")
+group_case_boro <- group_case_boro %>% 
+  select(group, subgroup, BK_CASE_COUNT, BX_CASE_COUNT, MN_CASE_COUNT, QN_CASE_COUNT, SI_CASE_COUNT) %>% 
+  rename("Brooklyn" = "BK_CASE_COUNT", "Bronx" = "BX_CASE_COUNT", "Manhattan" = "MN_CASE_COUNT" , 
+         "Queens" = "QN_CASE_COUNT", "Staten Island" = "SI_CASE_COUNT")
+
+# case count by date by borough for time series graph
+data_by_date <- read.csv("https://raw.githubusercontent.com/nychealth/coronavirus-data/master/trends/data-by-day.csv")
+data_by_date <- data_by_date %>% select(date_of_interest, CASE_COUNT, BX_CASE_COUNT, BK_CASE_COUNT, MN_CASE_COUNT, QN_CASE_COUNT, SI_CASE_COUNT) %>%
+  rename("Brooklyn" = "BK_CASE_COUNT", "Bronx" = "BX_CASE_COUNT", "Manhattan" = "MN_CASE_COUNT" , "Queens" = "QN_CASE_COUNT", "Staten Island" = "SI_CASE_COUNT")
+data_by_date$date_of_interest <- as.Date(data_by_date$date_of_interest, format = "%m/%d/%Y")
+
+
--- app/server.R
+++ app/server.R
@@ -6,148 +6,92 @@
 #
 #    http://shiny.rstudio.com/
 #
-#-------------------------------------------------App Server----------------------------------
-library(viridis)
-library(dplyr)
-library(tibble)
-library(tidyverse)
-library(shinythemes)
-library(sf)
-library(RCurl)
-library(tmap)
-library(rgdal)
-library(leaflet)
-library(shiny)
-library(shinythemes)
-library(plotly)
-library(ggplot2)
-#can run RData directly to get the necessary date for the app
-#global.r will enable us to get new data everyday
-#update data with automated script
-source("global.R") 
-#load('./output/covid-19.RData')
+
+source("global.R")
+
+
 shinyServer(function(input, output) {
-#----------------------------------------
-#tab panel 1 - Home Plots
-#preapare data for plot
-output$case_overtime <- renderPlotly({
-    #determin the row index for subset
-    req(input$log_scale)
-    end_date_index <- which(date_choices == input$date)
-    #if log scale is not enabled, we will just use cases
-    if (input$log_scale == FALSE) {
-        #render plotly figure
-        case_fig <- plot_ly()
-        #add comfirmed case lines
-        case_fig <- case_fig %>% add_lines(x = ~date_choices[1:end_date_index], 
-                             y = ~as.numeric(aggre_cases[input$country,])[1:end_date_index],
-                             line = list(color = 'rgba(67,67,67,1)', width = 2),
-                             name = 'Confirmed Cases')
-        #add death line 
-        case_fig <- case_fig %>% add_lines(x = ~date_choices[1:end_date_index],
-                               y = ~as.numeric(aggre_death[input$country,])[1:end_date_index],
-                               name = 'Death Toll')
-        #set the axis for the plot
-        case_fig <- case_fig %>% 
-            layout(title = paste0(input$country,'\t','Trend'),
-                   xaxis = list(title = 'Date',showgrid = FALSE), 
-                   yaxis = list(title = 'Comfirmed Cases/Deaths',showgrid=FALSE)
-                   )
-        }
-    #if enable log scale, we need to take log of the y values
-    else{
-        #render plotly figure
-        case_fig <- plot_ly()
-        #add comfirmed case lines
-        case_fig <- case_fig %>% add_lines(x = ~date_choices[1:end_date_index], 
-                                           y = ~log(as.numeric(aggre_cases[input$country,])[1:end_date_index]),
-                                           line = list(color = 'rgba(67,67,67,1)', width = 2),
-                                           name = 'Confirmed Cases')
-        #add death line 
-        case_fig <- case_fig %>% add_lines(x = ~date_choices[1:end_date_index],
-                                           y = ~log(as.numeric(aggre_death[input$country,])[1:end_date_index]),
-                                           name = 'Death Toll')
-        #set the axis for the plot
-        case_fig <- case_fig %>% 
-            layout(title = paste0(input$country,'<br>','\t','Trends'),
-                   xaxis = list(title = 'Date',showgrid = FALSE), 
-                   yaxis = list(title = 'Comfirmed Cases/Deaths(Log Scale)',showgrid=FALSE)
-            )
-    }
-    return(case_fig)
-        })
-#----------------------------------------
-#tab panel 2 - Maps
-data_countries <- reactive({
-    if(!is.null(input$choices)){
-        if(input$choices == "Cases"){
-            return(aggre_cases_copy)
-            
-        }else{
-            return(aggre_death_copy)
-        }}
-})
-
-#get the largest number of count for better color assignment
-maxTotal<- reactive(max(data_countries()%>%select_if(is.numeric), na.rm = T))    
-#color palette
-pal <- reactive(colorNumeric(c("#FFFFFFFF" ,rev(inferno(256))), domain = c(0,log(binning(maxTotal())))))    
+  
+#-------------------- Tab Home Page -----------------------------
     
-output$map <- renderLeaflet({
-    map <-  leaflet(countries) %>%
-        addProviderTiles("Stadia.Outdoors", options = providerTileOptions(noWrap = TRUE)) %>%
-        setView(0, 30, zoom = 3) })
-
-
-observe({
-    if(!is.null(input$date_map)){
-        select_date <- format.Date(input$date_map,'%Y-%m-%d')
-    }
-    if(input$choices == "Cases"){
-        #merge the spatial dataframe and cases dataframe
-        aggre_cases_join <- merge(countries,
-                                  data_countries(),
-                                  by.x = 'NAME',
-                                  by.y = 'country_names',sort = FALSE)
-        #pop up for polygons
-        country_popup <- paste0("<strong>Country: </strong>",
-                                aggre_cases_join$NAME,
-                                "<br><strong>",
-                                "Total Cases: ",
-                                aggre_cases_join[[select_date]],
-                                "<br><strong>")
-        leafletProxy("map", data = aggre_cases_join)%>%
-            addPolygons(fillColor = pal()(log((aggre_cases_join[[select_date]])+1)),
-                        layerId = ~NAME,
-                        fillOpacity = 1,
-                        color = "#BDBDC3",
-                        weight = 1,
-                        popup = country_popup) 
-    } else {
-        #join the two dfs together
-        aggre_death_join<- merge(countries,
-                                 data_countries(),
-                                 by.x = 'NAME',
-                                 by.y = 'country_names',
-                                 sort = FALSE)
-        #pop up for polygons
-        country_popup <- paste0("<strong>Country: </strong>",
-                                aggre_death_join$NAME,
-                                "<br><strong>",
-                                "Total Deaths: ",
-                                aggre_death_join[[select_date]],
-                                "<br><strong>")
-        
-        leafletProxy("map", data = aggre_death_join)%>%
-            addPolygons(fillColor = pal()(log((aggre_death_join[[select_date]])+1)),
-                        layerId = ~NAME,
-                        fillOpacity = 1,
-                        color = "#BDBDC3",
-                        weight = 1,
-                        popup = country_popup)
-        
-        }
+    output$timestamp <- renderText({
+      paste0("Updated On: ", as.character(nyc_total$NUMBER_OF_NYC_RESIDENTS[nyc_total$MEASURE == "DATE_UPDATED"]))
+      })
+    
+    
+    output$NYCtotal <- renderInfoBox({
+      infoBox(
+        title = tags$p("Total NYC Cases", style = "font-size: 100%;"),
+        value = tags$p(prettyNum(nyc_total$NUMBER_OF_NYC_RESIDENTS[nyc_total$MEASURE == "NYC_CASE_COUNT"], big.mark = ","), style = "font-size: 200%;"),
+        icon = icon("fas fa-bed"),
+        fill = T,
+        color = "navy")
+      })
+    
+    output$Boro <- renderInfoBox({
+      infoBox(
+        title = tags$p("Borough with Most Cases", style = "font-size: 100%;"),
+        value = tags$p(data_by_boro$BOROUGH_GROUP[data_by_boro$CASE_COUNT == max(data_by_boro$CASE_COUNT)], style = "font-size: 200%;"),
+        icon = icon("fas fa-user-times"),
+        fill = T,
+        color = "yellow")
+    })
+    
+    output$Zipcode <- renderInfoBox({
+      infoBox(
+        title = tags$p("Zip Code with Most Cases", style = "font-size: 100%;"),
+        value = tags$p(data_by_modzcta$MODIFIED_ZCTA[data_by_modzcta$COVID_CASE_COUNT == max(data_by_modzcta$COVID_CASE_COUNT)], style = "font-size: 200%;"),
+        icon = icon("fas fa-user-times"),
+        fill = T,
+        color = "yellow")
+    })
+    
+    output$Boro_safe <- renderInfoBox({
+      infoBox(
+        title = tags$p("Borough with Least Cases", style = "font-size: 100%;"),
+        value = tags$p(data_by_boro$BOROUGH_GROUP[data_by_boro$CASE_COUNT == min(data_by_boro$CASE_COUNT)], style = "font-size: 200%;"),
+        icon = icon("fas fa-user-check"),
+        fill = T,
+        color = "green")
+    })
+    
+    output$Zipcode_safe <- renderInfoBox({
+      infoBox(
+        title = tags$p("Zip Code with Least Cases", style = "font-size:100%:"),
+        value = tags$p(data_by_modzcta$MODIFIED_ZCTA[data_by_modzcta$COVID_CASE_COUNT == min(data_by_modzcta$COVID_CASE_COUNT)], style = "font-size: 200%;"),
+        icon = icon("fas fa-user-check"),
+        fill = T,
+        color = "green")
     })
 
 
-})
+    
+#-------------------- Tab Neighborhood Analysis -----------------
+    
+    # construct plotly pie charts for Age, Race, and Gender 
+    fig <- plot_ly()
+    fig <- fig %>% add_pie(data = group_case_boro[group_case_boro$group == 'Age', ], labels = ~subgroup, values = ~get(input$boroname),
+                           name = "AGE", title = "AGE", domain = list(row=0, column=0))
+    fig <- fig %>% add_pie(data = group_case_boro[group_case_boro$group == 'Race/ethnicity', ], labels = ~subgroup, values = ~get(input$boroname), 
+                           name = "RACE", title = "RACE", domain = list(row=0, column=1))
+    fig <- fig %>% add_pie(data = group_case_boro[group_case_boro$group == 'Sex', ], labels = ~subgroup, values = ~get(input$boroname),
+                           name = "GENDER", title = "GENDER", domain = list(row=0, column=2))
+    fig <- fig %>% layout(title = "Percentage of Case Count by Category", showlegend = F, grid = list(rows=1, columns=3),
+                          paper_bgcolor="transparent",
+                          xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+                          yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+    
+    output$pie_chart <- renderPlotly({ggplotly(fig)})
+    
+    
+    # construct plotly time series chart for borough's case count
+    fig1 <- plot_ly(data = data_by_date, x = ~date_of_interest, y = ~get(input$boroname), type ='scatter', mode = 'lines', line = list(color = 'orange'))
+    fig1 <- fig1 %>% layout(
+      title = "Case Count Over Time",
+      paper_bgcolor="transparent",
+      xaxis = list(title = "Date", rangeslider = list(type = "date"), showgrid = FALSE),
+      yaxis = list(title = "Case Count", showgrid = FALSE))
+    
+    output$time_series_plot <- renderPlotly({ggplotly(fig1)})
+
+}) # closing the entire code block
